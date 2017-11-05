4695c1cd8887c865d67dad13e8f0ae123ab4939b
Dayna Gordon <daynagordon@gmail.com>
2017-11-04 22:19:16 -0400

    Add Tile functionality

---
+++ tile.rb
@@ -0,0 +1,45 @@
+require 'colorize'
+
+class Tile
+  
+  attr_reader :is_a_bomb
+  
+  def initialize bomb = false
+    @is_a_bomb = bomb
+    @is_revealed = false
+    @is_flagged = false
+    @value = "*"
+  end
+  
+  
+  def reveal adjacent_value = "_"
+    unless self.flagged?
+      @value = (@is_a_bomb ? "B".colorize(:red) : adjacent_value)
+      @is_revealed = true
+    end
+  end
+  
+  def toggle_flag
+    unless @is_revealed
+      @is_flagged = !@is_flagged
+      @value = "F".colorize(:blue) if @is_flagged
+    end
+    @is_flagged
+  end
+  
+  def flagged?
+    @is_flagged
+  end
+  
+  def revealed?
+    @is_revealed
+  end
+  
+  def to_s
+    "#{@value}"
+  end
+  
+  def inspect
+    "value: #{@value}\nbomb?: #{self.is_a_bomb}"
+  end
+end
