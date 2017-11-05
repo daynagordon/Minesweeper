e933a205dc41b02fc0c97668bfa0db29f2d0e437
Dayna Gordon <daynagordon@gmail.com>
2017-11-04 23:34:46 -0400

    Define initialize

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,4 +1,21 @@
 class Minesweeper
+  
+  def initialize width = 4, length = 4
+    @board = Board.new(width, length)
+  end
+  
+  def run
+    
+  end
+  
+  def solved?
+  end
+  
+  def get_pos
+  end
+  
+  def parse_pos
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
