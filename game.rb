7c161eb06c716516f2606c1546696554f824ee29
Dayna Gordon <daynagordon@gmail.com>
2017-11-06 09:45:54 -0500

    Define game functionality

--- game.rb
+++ game.rb
@@ -1,19 +1,69 @@
+require_relative 'board'
+
 class Game
   
   def initialize width = 4, length = 4
     @board = Board.new(width, length)
+    @width = width
+    @length = length
   end
   
   def run
+    @board.populate
     
+    until @board.over?
+      @board.render
+      
+      option, pos = self.get_input
+      case option
+      when 'r', 'R'
+        @board.reveal( pos )
+      when 'F', 'f'
+        @board.toggle_flag( pos )
+      end
+    end
+    
+    @board.render
+    if @board.lost?
+      puts "Bad luck, that."
+    else
+      puts "Way to go, champ!"
+    end
   end
   
-  def solved?
+  def get_input
+    puts "Please enter your choice as option,x coordinate, y coordinate"
+    print "e.g. \"r,x,y\" or \"f,x,y\" > "
+    input = parse_input(gets.chomp)
+    
+    until valid? input
+      begin
+        puts "Check your formatting: \"r,x,y\" or \"f,x,y\""
+        input = parse_input(gets.chomp)
+      rescue => error
+        puts "#{error}"
+        print "> "
+      end
+    end
+    input
   end
   
-  def get_pos
+  def parse_input input
+    input = input.split(",")
+    option  = (input.size == 2 ? 'r' : input.shift)
+    [option, input.map{ |char| Integer(char) }]
   end
   
-  def parse_pos
+  def valid? input
+    ['r','R','f','F'].include?( input.first ) && valid_pos?( input.last )
+  end
+  
+  def valid_pos? pos
+    x, y = pos
+    (0...@width).cover?(x) && (0...@length).cover?(y) 
+  end
+  
+  def over?
+    @board.lost? || @board.won?
   end
 end
