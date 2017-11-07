6336229857ef75ed5d8842681cf2563eecfc1b1d
Dayna Gordon <daynagordon@gmail.com>
2017-11-06 19:17:41 -0500

    Allow loading of previous game from command line

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,11 +1,21 @@
 require_relative 'game'
+require 'yaml'
 
 if __FILE__ == $PROGRAM_NAME
+  
+  filename = ARGV.shift
+  
   easy = {
     num_bombs: 5,
     width: 9,
     length: 9
   }
-  g = Game.new easy
+  
+  if filename
+    g = YAML.load(File.read(filename))
+  else
+    g = Game.new easy  
+  end
+  
   g.run
 end
